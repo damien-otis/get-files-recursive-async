c32c6c20712af970bb12cbab85877e7cdf36c3a3
Damien Otis <sourcetree@dmtmix.com>
2016-01-02 00:04:50 -0800

    regex for filename matching

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function getFilesRecursiveAsync(folder,callback,filetypes,progress){
   var files = [];
 
   var filetype_regexp = [];
+  filetypes = filetypes || [];
   for (var i=0;i<filetypes.length;i++){
-    filetype_regexp.push(new RegExp(filetypes[i]+'$','i'))
+    filetype_regexp.push(new RegExp(filetypes[i],'i'))
   }
 
   function recurseFolders(thisfolder,thiscallback){
@@ -29,7 +30,7 @@ function getFilesRecursiveAsync(folder,callback,filetypes,progress){
             if (stats.isDirectory()){
               recurseFolders(this_path,endIterate)
             } else {
-              if (!filetypes || (filetypes && filetypes.length > 0 && fileMatch(this_path)) ){
+              if (filetypes.length == 0 || (filetypes.length > 0 && fileMatch(this_path)) ){
                 arr.push(this_path);
               }
               endIterate(null);
@@ -58,6 +59,7 @@ function getFilesRecursiveAsync(folder,callback,filetypes,progress){
   recurseFolders(folder,callback);
 
   function fileMatch(file){
+    if (filetype_regexp.length == 0){console.log("true",file);return true}
     //var ext = file.split(".").pop().toLowerCase()
     for (var i=0;i<filetype_regexp.length;i++){
       if (file.match(filetype_regexp[i]) !== null) {
